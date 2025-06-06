431e153703d91731e21fdb557e1353348c54b434
Fyne5 <phungthanhquang@gmail.com>
2025-06-06 12:23:55 +0700

    Simplerxy Wireguard

---
+++ docker/proxy-2-wireguard/README.md
@@ -0,0 +1,23 @@
+## Mô tả / Describes
+Simplerxy kết hợp WireguardVPN trong từng container. Mục đích xài cho cái gì thì tự người dùng quyết định. Có thể gợi ý là: WPAD.DAT
+
+Simpler bundles Wireguard VPN in each container. What you use it for is up to you. Suggestions include: WPAD.DAT
+
+## Cách chạy / How to run
+Chỉnh nội dung wireguard.conf và simplerxy.conf cho phù hợp các yếu tố. Chạy tạo ra container.
+
+Edit wireguard.conf and simplerxy.conf to suit your needs. Run the container creation.
+```
+chmod +x START.sh
+./START.sh <your container name>
+```
+
+## Cách xài / How to use
+Từ đâu đó, chạy thử 2 lệnh curl sau để kiểm tra (giả bộ máy ảo chạy Simplerxy Wireguad có IP là 192.168.200.105)
+
+From somewhere, run the following 2 curl commands to test (pretend the virtual machine running Simplerxy Wireguad has IP 192.168.200.105)
+
+```
+curl http://ipinfo.io/ip
+curl http://ipinfo.io/ip -x http://192.168.200.105:3979
+```
---
+++ docker/proxy-2-wireguard/START.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#How to use: ./START <NAME_CONTAINER>
+
+WIREGUARDCONF="./wireguard.conf"
+SIMPLERXYCONF="./simplerxy.conf"
+SIMPLERXYPORT="3979"
+
+if [[ -z "$#" || "$#" -ne 1 ]]; then
+  exit 0
+ else
+
+cat << EOF > Dockerfile_simplerxy
+FROM alpine:3.22.0
+RUN apk update && apk upgrade -a
+RUN apk add --no-cache iptables iproute2 wireguard-tools-wg-quick wireguard-tools-openrc curl brotli-libs dumb-init
+RUN mkdir -p /ENTRYPOINT
+WORKDIR /ENTRYPOINT
+#COPY Entrypoint.sh Simplerxy .
+#RUN chmod +x Simplerxy Entrypoint.sh
+COPY Simplerxy .
+RUN chmod +x Simplerxy
+EXPOSE 3979
+ENTRYPOINT ["/usr/bin/dumb-init", "--"]
+CMD ["./Entrypoint.sh"]
+EOF
+
+cat << EOF > Entrypoint.sh
+#!/bin/bash
+cleanup_wireguard() {
+   wg-quick down $1
+   exit 0
+}
+trap cleanup_wireguard SIGTERM SIGINT
+wg-quick up $1
+./Simplerxy
+EOF
+chmod 777 Entrypoint.sh
+
+  if [[ -z `docker images | grep simplerxy-$1` ]]; then
+   if [[ ! -f Simplerxy ]]; then
+    wget -q https://github.com/Fyne5/Simplerxy/releases/download/0.0.1/Simplerxy-linux-amd64 -O Simplerxy
+   fi
+   docker build -t simplerxy-$1 -f Dockerfile_simplerxy .
+  fi
+
+  docker run -d --name $1 -p $SIMPLERXYPORT:3979 \
+   --cap-add NET_ADMIN --cap-add SYS_MODULE --privileged \
+   -v $WIREGUARDCONF:/etc/wireguard/$1.conf -v $SIMPLERXYCONF:/ENTRYPOINT/config.conf -v ./Entrypoint.sh:/ENTRYPOINT/Entrypoint.sh \
+   --restart unless-stopped simplerxy-$1
+
+  echo "docker stop $1;docker rm $1;docker run -d --name $1 -p $SIMPLERXYPORT:3979 \
+   --cap-add NET_ADMIN --cap-add SYS_MODULE --privileged \
+   -v $WIREGUARDCONF:/etc/wireguard/$1.conf -v $SIMPLERXYCONF:/ENTRYPOINT/config.conf -v ./Entrypoint.sh:/ENTRYPOINT/Entrypoint.sh \
+   --restart unless-stopped simplerxy-$1" > DOCKER.sh
+
+  rm -rf Dockerfile_simplerxy Simplerxy
+
+  sleep 3
+  echo "Da xong, vui long kiem tra lai"
+  docker ps -a | grep $1
+  ifconfig $1
+  docker logs -f $1
+fi
